a11bf4b7b25a84349ff1c525a0e332179e2f08be
karinmae <kmaennersdorfer@gmx.at>
2014-04-10 17:07:02 +0200

    Basic UI ViewModels

---
+++ MicroErp_01/ViewModels/ContactSearchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroErp_01.ViewModels
+{
+    public class ContactSearchViewModel : SearchViewModel
+    {
+        public override void Search()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override GridDisplayConfiguration DisplayedColumns
+        {
+            get { return null; }
+        }
+    }
+}
--- MicroErp_01/ViewModels/MainWindowViewModel.cs
+++ MicroErp_01/ViewModels/MainWindowViewModel.cs
@@ -11,5 +11,31 @@ namespace MicroErp_01.ViewModels
         public MainWindowViewModel()
         {
         }
+
+        private InvoiceSearchViewModel _InvoiceSearchViewModel;
+        public InvoiceSearchViewModel InvoiceSearchViewModel
+        {
+            get
+            {
+                if (_InvoiceSearchViewModel == null)
+                {
+                    _InvoiceSearchViewModel = new InvoiceSearchViewModel();
+                }
+                return _InvoiceSearchViewModel;
+            }
+        }
+
+        private ContactSearchViewModel _ContactSearchViewModel;
+        public ContactSearchViewModel ContactSearchViewModel
+        {
+            get
+            {
+                if (_ContactSearchViewModel == null)
+                {
+                    _ContactSearchViewModel = new ContactSearchViewModel();
+                }
+                return _ContactSearchViewModel;
+            }
+        }
     }
 }
