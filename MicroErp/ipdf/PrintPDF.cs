fb4bc2b75020bbc0844e068912e859e116bc4e6f
Terra7 <tplacho@outlook.com>
2014-06-12 12:04:06 +0200

    Print pdf

--- MicroErp/ipdf/PrintPDF.cs
+++ MicroErp/ipdf/PrintPDF.cs
@@ -63,12 +63,10 @@ namespace MicroERP.ipdf
             cb.SetFontAndSize(font_calibri, 12);
             cb.SetTextMatrix(50, 740);
             cb.SetColorFill(new BaseColor(0, 0, 0));
-            cb.ShowText("MicroERP");
+            cb.ShowText(bill.Vorname + " " + bill.Nachname);
             cb.SetTextMatrix(50, 725);
-            cb.ShowText("Stenersgata 10 ");
-            cb.SetTextMatrix(50, 710);
-            cb.ShowText("0184 Oslo");
-            cb.SetFontAndSize(font_Fett, 16);
+            cb.ShowText(bill.Billingadress);
+            
             cb.SetTextMatrix(50, 600);
             cb.ShowText("Rechnung Nr. " + bill.Faelligkeit.ToString()); 
             cb.SetFontAndSize(font_calibri, 9);
@@ -81,6 +79,7 @@ namespace MicroERP.ipdf
             doc.Add(p1);
 
             DataTable filledTable = new DataTable();
+            filledTable.Columns.Add("Pos");
             filledTable.Columns.Add("Menge");
             filledTable.Columns.Add("Artikel");
             filledTable.Columns.Add("Stückpreis");
@@ -99,6 +98,7 @@ namespace MicroERP.ipdf
                 netto1 = menge * stk;
                 Ust = Convert.ToInt32(bill.Ust1);
                 brutto1 = netto1 * ((Ust / 100) + 1);
+                row["Pos"] = "1";
                 row["Menge"] = bill.Menge1;
                 row["Artikel"] = bill.Artikel1;
                 row["Stückpreis"] = bill.Stueckpreis1;
@@ -113,6 +113,7 @@ namespace MicroERP.ipdf
                 netto2 = menge * stk;
                 Ust = Convert.ToInt32(bill.Ust2);
                 brutto2 = netto2 * ((Ust / 100) + 1);
+                row["Pos"] = "2";
                 row2["Menge"] = bill.Menge2;
                 row2["Artikel"] = bill.Artikel2;
                 row2["Stückpreis"] = bill.Stueckpreis2;
@@ -127,6 +128,7 @@ namespace MicroERP.ipdf
                 netto3 = menge * stk;
                 Ust = Convert.ToInt32(bill.Ust3);
                 brutto3 = netto3 * ((Ust / 100) + 1);
+                row["Pos"] = "3";
                 row3["Menge"] = bill.Menge3;
                 row3["Artikel"] = bill.Artikel3;
                 row3["Stückpreis"] = bill.Stueckpreis3;
@@ -167,41 +169,6 @@ namespace MicroERP.ipdf
             pricing.SetWidths(new float[] { 80, (float)13.3 });
             pricing.SpacingAfter = 40;
 
-
-            //var pricingList = new List<KeyValuePair<string, float>>();
-            //pricingList.Add(new KeyValuePair<string, float>("Gesamt Netto:", sumNetto));
-            //if (USt20)
-            //{
-            //    pricingList.Add(new KeyValuePair<string, float>("20% USt:", sumUSt20)); // Werte einfügen
-            //}
-            //if (USt10)
-            //{
-            //    pricingList.Add(new KeyValuePair<string, float>("10% USt:", sumUSt10));
-            //}
-            //pricingList.Add(new KeyValuePair<string, float>("Gesamt Brutto:", sumBrutto));
-
-            //foreach (KeyValuePair<string, float> pair in pricingList)
-            //{
-            //    PdfPCell cell1 = new PdfPCell();
-            //    PdfPCell cell2 = new PdfPCell();
-            //    cell1.HorizontalAlignment = Element.ALIGN_RIGHT;
-            //    cell2.HorizontalAlignment = Element.ALIGN_RIGHT;
-            //    cell1.Border = Rectangle.NO_BORDER;
-            //    cell2.Border = Rectangle.NO_BORDER;
-
-
-            //    if (pair.Key == "Gesamt Brutto:")
-            //    {
-            //        cell1.Phrase = new Phrase(pair.Key, new Font(font_calibri, 11, Font.BOLD));
-            //    }
-            //    else
-            //    {
-            //        cell1.Phrase = new Phrase(pair.Key, new Font(font_calibri, 11, Font.NORMAL));
-            //    }
-            //    cell2.Phrase = new Phrase(pair.Value.ToString() + "€", new Font(font_calibri, 11, Font.NORMAL));
-            //    pricing.AddCell(cell1);
-            //    pricing.AddCell(cell2);
-            //}
 
             paragraphPricing.Add(pricing);
             doc.Add(paragraphPricing);
